8840b8e914600f692421b908d1c85d5d19f9f5b9
Dylan Stone-Miller <dylan@MacBook-Pro.local>
2020-08-11 11:19:11 -0400

    Updates

---
+++ precache-manifest.8491c3c419c1293e54d03743a6d02b72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab742bdb7067dba203685604b4f35793",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4e332e8e23bd10dd1d74",
+    "url": "/portfolio/static/css/main.f31c674b.chunk.css"
+  },
+  {
+    "revision": "ab713d96f058cda47b4d",
+    "url": "/portfolio/static/js/2.b8dbf444.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.b8dbf444.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e332e8e23bd10dd1d74",
+    "url": "/portfolio/static/js/main.aa5c087a.chunk.js"
+  },
+  {
+    "revision": "eb0aafffcb038f123bf0",
+    "url": "/portfolio/static/js/runtime-main.255623fe.js"
+  },
+  {
+    "revision": "cdaa2e73f2a0f3006de0efe1061b58aa",
+    "url": "/portfolio/static/media/IMG_5263.cdaa2e73.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.fa3f32cb588a1b2caa24b3bf9ea1dc2a.js"
+  "/portfolio/precache-manifest.8491c3c419c1293e54d03743a6d02b72.js"
 );
 
 self.addEventListener('message', (event) => {
